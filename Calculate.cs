45a3fc3b4964f1b339da438dbd331344dcb426c6
CodeImproverTestUser <77671164+CodeImproverTestUser@users.noreply.github.com>
2021-03-29 12:16:51 +0200

    Update Calculate.cs

--- Calculate.cs
+++ Calculate.cs
@@ -4,8 +4,8 @@ namespace HelloWorld
 {
     public class CalculateClass
     {
-        int l1, l2, l3, l4, l5, l6, l7, l8;
-        public CalculateClass(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8)
+        int l1, l2, l3, l4, l5, l6, l7;
+        public CalculateClass(int v1, int v2, int v3, int v4, int v5, int v6, int v7)
         {
             l1 = v1;
             l2 = v2;
@@ -13,8 +13,7 @@ namespace HelloWorld
             l4 = v4;
             l5 = v5;
             l6 = v6;
-            l7 = v7;
-            l8 = v8;
+            l7 = v7;            
             if (l9 == 0)
             {
                 if (l1 > l2) 
@@ -50,33 +49,7 @@ namespace HelloWorld
                 l6 = l6 + 1;
                 }  
             }
-            else
-            {
-                if (l1 > l2) 
-                {
-                    return;
-                }
-                else
-                {
-                    l1 =l1 + 1; 
-                }
-                if (l2 > l3) 
-                {
-                    return;
-                }
-                else
-                { 
-                l2 = l2 + 1;
-                } 
-                if (l3 > l4) 
-                {
-                    return;
-                }
-                else
-                { 
-                l3 = l3 + 1;
-                }  
-            }                      
+                         
         }
         public int Sum()
         {
